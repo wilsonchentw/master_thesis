9ab58f01e9ca41c44a66bf6504cb53423eaae1d8
zxm20243 <zxm20243@gmail.com>
2015-02-06 17:23:33 +0800

    only output uchar data

--- preprocess.cpp
+++ preprocess.cpp
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
     while(fin >> path >> label){
         cv::Mat image, norm_img;
         image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
-        cv::resize(image, norm_img, cv::Size(256, 256));
+        cv::resize(image, norm_img, cv::Size(2, 2));
         norm_img = norm_img.reshape(1, 1);
 
         fout << label;
         for( int i=0; i<norm_img.total(); i++ ){
             if( norm_img.at<uchar>(i)!=0 )
-                fout << " " << i+1 << ":" << (double)norm_img.at<uchar>(i)/255;
+                fout << " " << i+1 << ":" << norm_img.at<uchar>(i);
         }
         fout << std::endl;
 
