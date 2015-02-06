e63f661ce0d0c438ba0b3905166cba4786f6a4ff
zxm20243 <zxm20243@gmail.com>
2015-02-06 14:31:42 +0800

    script do everything

---
+++ go.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
+python3 split_dataset.py ../50data train_list test_list 5
+./preprocess train_list train_libsvm
+rm train_list test_list preprocess
