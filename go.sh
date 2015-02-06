8b2ed996780cc77b26cc3b4102e20692428e5166
zxm20243 <zxm20243@gmail.com>
2015-02-06 14:48:09 +0800

    modify precision to reduce file size

--- go.sh
+++ go.sh
@@ -3,4 +3,4 @@
 g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
 python3 split_dataset.py ../50data train_list test_list 5
 ./preprocess train_list train_libsvm
-rm train_list test_list preprocess
+rm preprocess
--- preprocess.cpp
+++ preprocess.cpp
@@ -12,20 +12,20 @@ int main(int argc, char **argv)
     std::string path;
     std::fstream fin(argv[1], std::fstream::in);
     std::fstream fout(argv[2], std::fstream::out);
+
+    fout.precision(4);
     while(fin >> path >> label){
         cv::Mat image, norm_img;
         image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
-        cv::resize(image, norm_img, cv::Size(128, 128));
+        cv::resize(image, norm_img, cv::Size(2, 2));
         norm_img = norm_img.reshape(1, 1);
 
         fout << label;
         for( int i=0; i<norm_img.total(); i++ ){
             if( norm_img.at<uchar>(i)!=0 )
-                fout << " " << i+1 << ":" << norm_img.at<uchar>(i);
+                fout << " " << i+1 << ":" << (double)norm_img.at<uchar>(i)/255;
         }
         fout << std::endl;
-
-        std::cout << path << std::endl;
     }
     fin.close();
 
