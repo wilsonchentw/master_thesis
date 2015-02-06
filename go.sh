f280caafb9dcce681958864d0e9b5a56f7f5547c
zxm20243 <zxm20243@gmail.com>
2015-02-07 03:36:21 +0800

    shell parallel execute

--- go.sh
+++ go.sh
@@ -13,12 +13,15 @@ python3 split_dataset.py ../$DATASET\
     -v 4 1 1
 
 g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
-./preprocess ${TRAIN}.list ${TRAIN}.dat
-./preprocess ${VAL}.list ${VAL}.dat
-./preprocess ${TEST}.list ${TEST}.dat 
+
+./preprocess ${TRAIN}.list ${TRAIN}.dat &
+./preprocess ${VAL}.list ${VAL}.dat &
+./preprocess ${TEST}.list ${TEST}.dat &
+wait
+ 
 ${LIBSVM_PATH}/svm-scale -l 0 -u 1 -s ${TRAIN}.range ${TRAIN}.dat > ${TRAIN}.scale.dat
-${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${VAL}.dat   > ${VAL}.scale.dat
-${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${TEST}.dat  > ${TEST}.scale.dat
+${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${VAL}.dat   > ${VAL}.scale.dat &
+${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${TEST}.dat  > ${TEST}.scale.dat &
 
 python ${LIBSVM_PATH}/tools/grid.py ${TRAIN}.scale.dat
 
