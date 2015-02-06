6b698a61d50e211467e022916f9d9a3dbdf13b56
zxm20243 <zxm20243@gmail.com>
2015-02-07 03:24:18 +0800

    grid for parameter

--- go.sh
+++ go.sh
@@ -10,14 +10,17 @@ TEST=${DATASET}_test
 
 python3 split_dataset.py ../$DATASET\
     -f ${TRAIN}.list ${VAL}.list ${TEST}.list\
-    -v 1 98 1
+    -v 4 1 1
 
 g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
 ./preprocess ${TRAIN}.list ${TRAIN}.dat
-#./preprocess ${VAL}.list ${VAL}.dat
+./preprocess ${VAL}.list ${VAL}.dat
 ./preprocess ${TEST}.list ${TEST}.dat 
-${LIBSVM_PATH}/svm-scale -l 0 -u 1 -s ${TRAIN}.range > ${TRAIN}.scale.dat
-#${LIBSVM_PATH}/svm-scale -l 0 -u 1 -s ${VAL}.range > ${VAL}.scale.dat
-${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range > ${TEST}.scale.dat
+${LIBSVM_PATH}/svm-scale -l 0 -u 1 -s ${TRAIN}.range ${TRAIN}.dat > ${TRAIN}.scale.dat
+${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${VAL}.dat   > ${VAL}.scale.dat
+${LIBSVM_PATH}/svm-scale -l 0 -u 1 -r ${TRAIN}.range ${TEST}.dat  > ${TEST}.scale.dat
+
+python ${LIBSVM_PATH}/tools/grid.py ${TRAIN}.scale.dat
 
 rm preprocess
+ls --color -lah
