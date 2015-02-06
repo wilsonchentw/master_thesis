45312824989ef25eed1937d08396092cf9d41d93
zxm20243 <zxm20243@gmail.com>
2015-02-06 17:10:58 +0800

    Use libsvm/tools/subset.py to split

--- go.sh
+++ go.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+DATASET = 50data
+FOLD = 100
+
 g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
-python3 split_dataset.py ../50data train_list test_list 5
-./preprocess train_list train_libsvm
-rm preprocess
+
+#python3 split_dataset.py ../$DATASET ${DATASET}_train.list ${DATASET}_test.list
+#./preprocess ${DATASET}_train.list ${DATASET}_train.libsvm
+#./preprocess ${DATASET}_test.list ${DATASET}_test.libsvm
+
+#./preprocess test_list test_libsvm
+#../libsvm/svm-scale -l 0 -u 1 -s test_libsvm.param test_libsvm  > test_libsvm.scale
+#../libsvm/svm-scale -r test_libsvm.param test_libsvm > test_libsvm.scale
+#rm preprocess
