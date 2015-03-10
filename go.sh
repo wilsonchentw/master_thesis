433a831e5d3674dcd43d7c76de1e88c59f923022
zxm20243 <zxm20243@gmail.com>
2015-03-11 00:42:00 +0800

    modify script for rename im2svm

--- go.sh
+++ go.sh
@@ -12,10 +12,10 @@ python3 split_dataset.py ../$DATASET\
     -f ${TRAIN}.list ${VAL}.list ${TEST}.list\
     -v 4 1 1
 
-g++ preprocess.cpp $(pkg-config --cflags --libs opencv) -o preprocess
-./preprocess ${TRAIN}.list ${TRAIN}.dat &
-./preprocess ${VAL}.list ${VAL}.dat &
-./preprocess ${TEST}.list ${TEST}.dat &
+g++ im2svm.cpp $(pkg-config --cflags --libs opencv) -o im2svm
+./im2svm ${TRAIN}.list ${TRAIN}.dat &
+./im2svm ${VAL}.list ${VAL}.dat &
+./im2svm ${TEST}.list ${TEST}.dat &
 wait
 
 cat ${TRAIN}.dat ${VAL}.dat > ${TRAIN}.dat
@@ -23,5 +23,5 @@ rm ${VAL}.dat
 python ${LIBSVM_PATH}/tools/grid.py ${TRAIN}.dat
 
 ## Clean
-rm preprocess
+rm im2svm
 ls --color -lah
