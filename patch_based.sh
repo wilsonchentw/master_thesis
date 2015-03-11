53a3e98a31b3a1e737e5a3b22bae6dd7f19f5a61
zxm20243 <zxm20243@gmail.com>
2015-03-12 04:34:38 +0800

    modify instance for local test

--- patch_based.sh
+++ patch_based.sh
@@ -12,11 +12,10 @@ rm 50data_*
 
 python3 split_dataset.py ../$DATASET\
     -f ${TRAIN}.list ${VAL}.list ${TEST}.list\
-    -v 4 1 1
+    -v 1 98 1
 
 g++ extract_patch.cpp $(pkg-config --cflags --libs opencv) -o extract_patch
 ./extract_patch ${TRAIN}.list ${TRAIN}_patch.dat
-./extract_patch ${VAL}.list   ${VAL}_patch.dat
 ./extract_patch ${TEST}.list  ${TEST}_patch.dat
 
 ## Clean
