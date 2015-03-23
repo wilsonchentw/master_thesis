024d638c423a1b03712e95c933b4cddd2673dd9a
zxm20243 <zxm20243@gmail.com>
2015-03-23 18:23:36 +0800

    modify parameter

--- feature_classify.sh
+++ feature_classify.sh
@@ -10,17 +10,17 @@ TEST=${DATASET}_test
 
 python3 split_dataset.py ../$DATASET\
     -f ${TRAIN}.list ${VAL}.list ${TEST}.list\
-    -v 4 1 1
+    -v 5 94 1
 
-g++ extract_features.cpp $(pkg-config --cflags --libs opencv) -o extract_features
-./extract_features ${TRAIN}.list ${TRAIN}_raw.dat
-./extract_features ${TEST}.list ${TEST}_raw.dat
+#g++ extract_features.cpp $(pkg-config --cflags --libs opencv) -o extract_features
+#./extract_features ${TRAIN}.list ${TRAIN}_raw.dat
+#./extract_features ${TEST}.list ${TEST}_raw.dat
 
-${LIBLINEAR_PATH}/train -q ${TRAIN}_raw.dat ${DATASET}_raw.model
+${LIBLINEAR_PATH}/train -c 0.01 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
 ${LIBLINEAR_PATH}/predict  ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
 ${LIBLINEAR_PATH}/predict  ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
 
-${LIBSVM_PATH}/svm-train -q ${TRAIN}_raw.dat ${DATASET}_raw.model
+${LIBSVM_PATH}/svm-train -c 0.01 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
 ${LIBSVM_PATH}/svm-predict  ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
 ${LIBSVM_PATH}/svm-predict  ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
 
