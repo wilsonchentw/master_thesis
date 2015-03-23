14b877fc4b1199ce8d33938622a7218aa2cf6891
zxm20243 <zxm20243@gmail.com>
2015-03-23 23:48:15 +0800

    rewrite

--- extract_features.cpp
+++ extract_features.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -42,14 +43,18 @@ Mat im2hist(Mat image)
     return hist;
 }
 
-void matToLibsvm(int label, Mat raw, std::fstream &fout){
+void matToLibsvm(int label, Mat &raw, std::fstream &fout){
+    CV_Assert(raw.channels()==1);
+
     Mat m;
     raw.convertTo(m, CV_64F);
+    MatIterator_<double> bgn = m.begin<double>();
+    MatIterator_<double> end = m.end<double>();
 
     fout << label;
-    for( int i=0; i<m.total(); i++ ){
-        if( m.at<double>(i) > 0 ){
-            fout << " " << i+1 << ":" << m.at<double>(i);
+    for( MatIterator_<double> it=bgn; it!=end; ++it ){
+        if( (*it) > 0 ){
+            fout << " " << (it-bgn)+1 << ":" << (*it);
         }
     }
     fout << endl;
@@ -66,16 +71,19 @@ int main(int argc, char **argv)
     while(fin >> path >> label){
         Mat image = imread(path, CV_LOAD_IMAGE_COLOR);
 
-        // normalize image
+        // Normalize image
         Mat norm_image;
         resize(image, norm_image, Size(IMG_HEIGHT, IMG_WIDTH));
-        norm_image = norm_image / 255;
+        norm_image.convertTo(norm_image, CV_32FC3, (double)1.0/255);
+        norm_image = norm_image.reshape(1);
         //norm_image = normalizeCrop(image);
 
-        // calculate histogram
+        // Calculate histogram
         //Mat hist = im2hist(norm_image);
 
+        // Output to libsvm training data file
         matToLibsvm(label, norm_image, fout);
+        //matToLibsvm(label, hist, fout);
     }
 
 
--- feature_classify.sh
+++ feature_classify.sh
@@ -10,20 +10,19 @@ TEST=${DATASET}_test
 
 python3 split_dataset.py ../$DATASET\
     -f ${TRAIN}.list ${VAL}.list ${TEST}.list\
-    -v 5 94 1
+    -v 1 4 1
 
-#g++ extract_features.cpp $(pkg-config --cflags --libs opencv) -o extract_features
-#./extract_features ${TRAIN}.list ${TRAIN}_raw.dat
-#./extract_features ${TEST}.list ${TEST}_raw.dat
+g++ extract_features.cpp $(pkg-config --cflags --libs opencv) -o extract_features
+./extract_features ${TRAIN}.list ${TRAIN}_raw.dat
+./extract_features ${TEST}.list ${TEST}_raw.dat
 
-${LIBLINEAR_PATH}/train -c 0.01 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
-${LIBLINEAR_PATH}/predict  ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
-${LIBLINEAR_PATH}/predict  ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
+${LIBLINEAR_PATH}/train -c 0.0001 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
+${LIBLINEAR_PATH}/predict ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
+${LIBLINEAR_PATH}/predict ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
 
-${LIBSVM_PATH}/svm-train -c 0.01 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
-${LIBSVM_PATH}/svm-predict  ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
-${LIBSVM_PATH}/svm-predict  ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
-
+#${LIBSVM_PATH}/svm-train -c 0.0001 -q ${TRAIN}_raw.dat ${DATASET}_raw.model
+#${LIBSVM_PATH}/svm-predict  ${TRAIN}_raw.dat ${DATASET}_raw.model ${TRAIN}.predict
+#${LIBSVM_PATH}/svm-predict  ${TEST}_raw.dat  ${DATASET}_raw.model ${TEST}.predict
 #${LIBSVM_PATH}/tools/grid.py -gnuplot null ${TRAIN}_raw.dat 
+
 #rm 50data* extract_features
-rm extract_features
