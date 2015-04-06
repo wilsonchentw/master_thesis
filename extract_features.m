cc0118dfb882368e6a6ba56e09840cfc53bac04e
zxm20243 <zxm20243@gmail.com>
2015-04-07 05:36:54 +0800

    encode with kmeans

--- extract_features.m
+++ extract_features.m
@@ -1,28 +1,56 @@
-function extract_features(image_list)
+function extract_features(train_list, test_list)
     % Add libsvm, liblinear, vlfeat library path
     run(fullfile('../vlfeat/toolbox/vl_setup'));
     addpath(fullfile('../libsvm/matlab'));
     addpath(fullfile('../liblinear/matlab'));
 
-    fd = fopen(image_list);
-    data = textscan(fd, '%s %d');
-    [path, label] = data{:};
+    % Parse training/testing path list
+    ftrain = fopen(train_list);
+    ftest = fopen(test_list);
+    train_data = textscan(ftrain, '%s %d');
+    test_data = textscan(ftest, '%s %d');
     fclose('all');
 
-    sift_descriptors = [];
-    for idx = 1:length(path):length(path)
-        image = imread(path{idx});
-        norm_image = normalize_image(image, [512 512], true);
+    % Generate codebook
+    [train_paths, train_labels] = train_data{:};
+    [dict, train_data] = generate_codebook(train_paths);
+
+    % Project testing data to dictionary
+    [test_paths, test_labels] = test_data{:};
+
+    %[~, code] = min(vl_alldist2(sifts, dict));
+end
+
+function features = encode_sift(dict, path_list)
+end
+
+function [dict, train_data] = generate_codebook(path_list)
+    sift_descriptors = {};
+    for idx = 1:length(path_list)/length(path_list)*3
+        % Normalize image to [h w], cut for central part if crop is true
+        image = imread(path_list{idx});
+        norm_size = [512 512];
+        norm_image = normalize_image(image, norm_size, true);
 
         % Extract SIFT descriptors
         gray_image = single(rgb2gray(norm_image));
-        [f, d] = vl_sift(gray_image);
-        sift_descriptors = [sift_descriptors d];
+        [f, sift_descriptors{idx}] = vl_sift(gray_image);
     end
 
-    % K-means clustering
-    num_clusters = 10;
-    [centers, assignments] = vl_kmeans(single(sift_descriptors), num_clusters);
+    % Generate codebook using K-means clustering on SIFT
+    dict_size = 1024;
+    dict = single(cell2mat(sift_descriptors));
+    [dict, encode] = vl_kmeans(dict, dict_size, 'Initialization', 'plusplus');
+
+    % Generate encoded features of training data
+    train_data = [];
+    for idx = 1:1:size(sift_descriptors, 2)
+        num = size(sift_descriptors{idx}, 2);
+        hist = vl_ikmeanshist(dict_size, encode(1:num));
+
+        train_data = [train_data hist]
+        encode(1:num) = [];
+    end
 end
 
 function norm_image = normalize_image(image, norm_size, crop)
