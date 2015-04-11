b4ac2449636de6a36e3eef3a5a81d0276c50a16e
zxm20243 <zxm20243@gmail.com>
2015-04-11 15:22:52 +0800

    refactor

--- feature_classify.m
+++ feature_classify.m
@@ -4,36 +4,74 @@ function feature_classify(image_list)
     addpath(fullfile('../liblinear/matlab'));
     addpath(fullfile('../libsvm/matlab'));
 
-    % Parameters setting
-    num_fold = 3;
-    c = 10.^[1:-1:-3];
-
-    % Split data for cross validation
+    % Parse image list into structure array    
     dataset = parse_image_list(image_list);
-    folds = cross_validation(dataset, num_fold);
 
-    % Classify
-    train_accs = zeros(num_fold, length(c));
-    test_accs = zeros(num_fold, length(c));
-    for v = 1:num_fold
-        features = extract_features(folds(v));
-        % Train by liblinear
-        parfor idx = 1:length(c)
-            model = train(double(folds(v).train.label), ...
-                          sparse(features.train), ...
+    % Extract image descriptors
+    norm_size = [64 64];
+    dataset(length(dataset)).sift = struct('sift', []);
+    parfor idx = 1:length(dataset)
+        % Read and preprocessing image
+        image = imread(dataset(idx).path);
+        norm_image = normalize_image(image, norm_size, true);
+
+        % Extract SIFT descriptors
+        dataset(idx).sift = extract_sift(image);
+    end
+
+    % For each fold, generate features by descriptors
+    num_fold = 5;
+    folds = cross_validation(dataset, num_fold);
+    c = 10.^[-3:1];
+    acc_list = cell(num_fold, length(c));
+    parfor v = 1:num_fold
+        % Generate SIFT features
+        dict_size = 1024/512;
+        train_sift = [folds(v).train.sift]';
+        test_sift = [folds(v).test.sift]';
+        sift = encode_sift(train_sift, test_sift, dict_size);
+
+        % Classify by SVM
+        train_label = double([folds(v).train.label]');
+        test_label = double([folds(v).test.label]');
+
+        for idx = 1:length(c)
+            model = train(train_label, sparse(sift.train), ...
                           ['-c ', num2str(c(idx)), ' -q'], 'col');
-            [~, train_acc, ~] = predict(double(folds(v).train.label), ...
-                                        sparse(features.train), model, ...
-                                        '-q', 'col');
-            [~, test_acc, ~] = predict(double(folds(v).test.label), ...
-                                       sparse(features.test), model, ...
-                                       '-q', 'col');
-            train_accs(v, idx) = train_acc(1);
-            test_accs(v, idx) = test_acc(1);
+            [~, acc, ~] = predict(test_label, ...
+                                  sparse(sift.test), model, '-q', 'col');
+            acc_list{v}(idx) = acc(1);
         end
-    end;
-    train_acc = sum(train_accs)/num_fold
-    test_acc = sum(test_accs)/num_fold
+    end
+    acc_list = [c; sum(cell2mat(acc_list))]
+    %save('sift.mat');
+end
+
+function features = encode_sift(train_sift, test_sift, dict_size);
+    % Generate codebook by K-means
+    [dict, train_asgn] = vl_ikmeans([train_sift.d], ...
+                                    dict_size, 'method', 'elkan');
+
+    % Encode image by codebook histogram
+    test_asgn = vl_ikmeanspush([test_sift.d], dict);
+    features.train = calc_kmeans_hists(train_asgn, [train_sift.n], dict_size);
+    features.test = calc_kmeans_hists(test_asgn, [test_sift.n], dict_size);
+end
+
+function norm_image = normalize_image(image, norm_size, crop)
+    if nargin < 3, crop=true; end
+
+    if not(crop)
+        norm_image = imresize(image, norm_size);
+    else
+        [height, width, channel] = size(image);
+        scale = max(norm_size./[height, width])+eps;
+        offset = floor(([height width]*scale - norm_size)/2);
+        x = offset(2)+1:offset(2)+norm_size(2);
+        y = offset(1)+1:offset(1)+norm_size(1);
+        resized_image = imresize(image, scale);
+        norm_image = resized_image(y, x, :);
+    end
 end
 
 function dataset = parse_image_list(image_list)
@@ -44,19 +82,28 @@ function dataset = parse_image_list(image_list)
     fclose(fd);
 end
 
+function descriptor = extract_sift(image)
+    descriptors = struct('f', [], 'd', [], 'n', 0);
+
+    % Extract SIFT descriptors
+    gray_image = single(rgb2gray(image));
+    [frames, local_descriptors] = vl_sift(gray_image);
+
+    descriptor.f = frames;
+    descriptor.d = local_descriptors;
+    descriptor.n = size(frames, 2);
+end
 
 function folds = cross_validation(dataset, num_fold)
-    folds(1:num_fold) = struct('train', struct('label', [], 'path', []), ...
-                               'test',  struct('label', [], 'path', []));
+    folds(1:num_fold) = struct('train', [], 'test', []);
     categories = unique([dataset.label]);
     for c = 1:length(categories)
-
-        % Select particular category
-        select = ([dataset.label] == categories(c));
-        list = dataset(select);
-
-        % Generate list of #testing_instance for each fold
+        % Select particular category, perform random permutation
+        list = dataset([dataset.label] == categories(c));
         len = length(list);
+        list = list(randperm(len));
+
+        % Calculate #test_case on each fold
         sample_fold = randsample(num_fold, mod(len, num_fold));
         test_nums(1:num_fold) = floor(len/num_fold);
         test_nums(sample_fold) = floor(len/num_fold)+1;
@@ -65,11 +112,20 @@ function folds = cross_validation(dataset, num_fold)
         for v = 1:num_fold
             test_list = list(1:test_nums(v));
             train_list = list(test_nums(v)+1:end);
-            folds(v).train.label = [folds(v).train.label; [train_list.label]'];
-            folds(v).test.label = [folds(v).test.label; [test_list.label]'];
-            folds(v).train.path = [folds(v).train.path; [{train_list.path}]'];
-            folds(v).test.path = [folds(v).test.path; [{test_list.path}]'];
+
+            folds(v).train = [folds(v).train; train_list];
+            folds(v).test = [folds(v).test; test_list];
             list = [train_list; test_list];
         end
     end
 end
+
+function hists = calc_kmeans_hists(assignment, num_descriptors, dict_size)
+    hists = zeros(dict_size, length(num_descriptors));
+    offset = cumsum(num_descriptors)-num_descriptors;
+    parfor idx = 1:length(num_descriptors)
+        v = assignment(offset(idx)+1:offset(idx)+num_descriptors(idx));
+        hists(:, idx) = vl_ikmeanshist(dict_size, v);
+    end
+end
+
