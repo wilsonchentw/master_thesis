1030159c695f86c9ee5388eb1b3a80206273823a
zxm20243 <zxm20243@gmail.com>
2015-04-13 16:59:36 +0800

    add LBP feature

--- feature_classify.m
+++ feature_classify.m
@@ -9,59 +9,83 @@ function feature_classify(image_list)
 
     % Extract image descriptors
     norm_size = [64 64];
-    dataset(length(dataset)).sift = struct('sift', []);
-    parfor idx = 1:length(dataset)
+    dataset(length(dataset)).sift = struct('sift', [], 'lbp', []);
+    for idx = 1:length(dataset)
         % Read and preprocessing image
         image = imread(dataset(idx).path);
         norm_image = normalize_image(image, norm_size, true);
 
         % Extract SIFT descriptors
-        dataset(idx).sift = extract_sift(image);
+        %dataset(idx).sift = extract_sift(norm_image);
+
+        % Extract LBP descriptors
+        %dataset(idx).lbp = extract_lbp(norm_image);
     end
 
     % For each fold, generate features by descriptors
-    num_fold = 3;
-    datasets = cross_validation(dataset, num_fold);
+    num_fold = 5;
+    folds = cross_validation(dataset, num_fold);
     parfor v = 1:num_fold
-        train_list = datasets(v).train;
-        test_list = datasets(v).test;
+        train_list = dataset(folds(v).train);
+        test_list = dataset(folds(v).test);
 
         labels = struct('train', [], 'test', []);
         labels.train = double([train_list.label]');
         labels.test = double([test_list.label]');
 
-        % Generate SIFT features
+        % Generate features by descriptors
         dict_size = 1024/512;
-        sift = encode_sift([train_list.sift]', [test_list.sift]', dict_size);
+        sift = kmeans_encode([train_list.sift]', [test_list.sift]', dict_size);
+        lbp = kmeans_encode([train_list.lbp]', [test_list.lbp]', dict_size);
 
         % Classify by linear SVM
         c = 10.^[1:-1:-3];
-        sift_acc(v+1, :) = linear_classify(sift, labels, c);
+        sift_acc(v, :) = linear_classify(sift, labels, c);
+        lbp_acc(v, :) = linear_classify(lbp, labels, c);
     end
 
     sift_acc = [10.^[1:-1:-3]; sum(sift_acc)]
+    lbp_acc = [10.^[1:-1:-3]; sum(lbp_acc)]
     %save('sift.mat');
 end
 
-function acc_list = linear_classify(features, labels, c)
-    acc_list = zeros(1, length(c));
-    parfor idx = 1:length(c)
-        model = train(labels.train, sparse(features.train), ...
-                      ['-c ', num2str(c(idx)), ' -q'], 'col');
-        [~, acc, ~] = predict(labels.test, ...
-                              sparse(features.test), model, '-q', 'col');
-        acc_list(idx) = acc(1);
-    end
+function descriptor = extract_sift(image)
+    descriptors = struct('f', [], 'd', [], 'n', 0);
+
+    % Extract SIFT descriptors
+    gray_image = single(rgb2gray(image));
+    [frames, local_descriptors] = vl_sift(gray_image);
+
+    descriptor.f = frames;
+    descriptor.d = local_descriptors;
+    descriptor.n = size(frames, 2);
 end
 
-function features = encode_sift(train_sift, test_sift, dict_size);
+function descriptor = extract_lbp(image)
+    descriptor = struct('d', [], 'n', 0);
+
+    gray_image = single(rgb2gray(image));
+    d = vl_lbp(gray_image, 8);
+    descriptor.d = reshape(permute(d, [3 1 2]), size(d, 3), []);
+    descriptor.d = [descriptor.d; 1-sum(descriptor.d)];
+    descriptor.n = size(descriptor.d, 2);
+end
+
+function features = kmeans_encode(train_descriptor, test_descriptor, dict_size)
     % Generate codebook by K-means
-    [dict, asgn] = vl_ikmeans([train_sift.d], dict_size, 'method', 'elkan');
-    train_encode = calc_kmeans_hists(asgn, [train_sift.n], dict_size);
+    if isa([train_descriptor.d], 'uint8') && isa([test_descriptor.d], 'uint8')
+        [dict, train_asgn] = vl_ikmeans([train_descriptor.d], ...
+                                        dict_size, 'method','elkan');
+        test_asgn = vl_ikmeanspush([test_descriptor.d], dict);
+    else
+        [dict, train_asgn] = vl_kmeans(double([train_descriptor.d]), ...
+                                       dict_size, 'algorithm', 'elkan');
+        [~, test_asgn] = min(vl_alldist2(dict, double([test_descriptor.d])));
+    end
 
     % Encode testing image by codebook histogram
-    asgn = vl_ikmeanspush([test_sift.d], dict);
-    test_encode = calc_kmeans_hists(asgn, [test_sift.n], dict_size);
+    train_encode = kmeans_hists(train_asgn, [train_descriptor.n], dict_size);
+    test_encode = kmeans_hists(test_asgn, [test_descriptor.n], dict_size);
 
     % Normalize quantized SIFT descriptor histogram
     %train_encode = bsxfun(@rdivide, train_encode, sum(train_encode));
@@ -71,7 +95,7 @@ function features = encode_sift(train_sift, test_sift, dict_size);
     features.test = test_encode;
 end
 
-function hists = calc_kmeans_hists(assignment, num_descriptors, dict_size)
+function hists = kmeans_hists(assignment, num_descriptors, dict_size)
     hists = zeros(dict_size, length(num_descriptors));
     offset = cumsum(num_descriptors)-num_descriptors;
     parfor idx = 1:length(num_descriptors)
@@ -104,24 +128,12 @@ function dataset = parse_image_list(image_list)
     fclose(fd);
 end
 
-function descriptor = extract_sift(image)
-    descriptors = struct('f', [], 'd', [], 'n', 0);
-
-    % Extract SIFT descriptors
-    gray_image = single(rgb2gray(image));
-    [frames, local_descriptors] = vl_sift(gray_image);
-
-    descriptor.f = frames;
-    descriptor.d = local_descriptors;
-    descriptor.n = size(frames, 2);
-end
-
 function folds = cross_validation(dataset, num_fold)
     folds(1:num_fold) = struct('train', [], 'test', []);
     categories = unique([dataset.label]);
     for c = 1:length(categories)
         % Select particular category, perform random permutation
-        list = dataset([dataset.label] == categories(c));
+        list = find([dataset.label] == categories(c))';
         len = length(list);
         list = list(randperm(len));
 
@@ -141,4 +153,16 @@ function folds = cross_validation(dataset, num_fold)
         end
     end
 end
+
+function acc_list = linear_classify(features, labels, c)
+    acc_list = zeros(1, length(c));
+    parfor idx = 1:length(c)
+        model = train(labels.train, sparse(features.train), ...
+                      ['-c ', num2str(c(idx)), ' -q'], 'col');
+        [~, acc, ~] = predict(labels.test, ...
+                              sparse(features.test), model, '-q', 'col');
+        acc_list(idx) = acc(1);
+    end
+end
+
 
