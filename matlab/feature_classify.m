64db66e5c9aec89e01641a56db40a26e3c5ff17c
zxm20243 <zxm20243@gmail.com>
2015-04-18 02:43:13 +0800

    add sparse coding, color histogram

--- matlab/feature_classify.m
+++ matlab/feature_classify.m
@@ -12,10 +12,13 @@ function feature_classify(image_list)
         norm_image = normalize_image(image, norm_size, true);
 
         % Extract SIFT descriptors
-        dataset(idx).sift = extract_sift(norm_image);
+        %dataset(idx).sift = extract_sift(norm_image);
 
         % Extract LBP descriptors
-        dataset(idx).lbp = extract_lbp(norm_image);
+        %dataset(idx).lbp = extract_lbp(norm_image);
+
+        % Extract color histograms
+        dataset(idx).color = extract_color_hist(norm_image);
     end
 
     % For each fold, generate features by descriptors
@@ -33,15 +36,19 @@ function feature_classify(image_list)
         dict_size = 1024/512;
         %sift = kmeans_encode([train_list.sift]', [test_list.sift]', dict_size);
         %lbp = kmeans_encode([train_list.lbp]', [test_list.lbp]', dict_size);
-        sift = sparse_encode([train_list.sift]', [test_list.sift]', dict_size);
+        %sift = sparse_encode([train_list.sift]', [test_list.sift]', dict_size);
+        %lbp = sparse_encode([train_list.lbp]', [test_list.lbp]', dict_size);
+        color.train = [train_list.color];
+        color.test = [test_list.color];
 
         % Classify by linear SVM
         c = 10.^[2:-1:-7];
-        sift_acc(v, :) = linear_classify(sift, labels, c);
+        color_acc(v, :) = linear_classify(color, labels, c);
+        %sift_acc(v, :) = linear_classify(sift, labels, c);
         %lbp_acc(v, :) = linear_classify(lbp, labels, c);
     end
-
-    sift_acc = [c; mean(sift_acc)]
+    color_acc = [c; mean(color_acc)]
+    %sift_acc = [c; mean(sift_acc)]
     %lbp_acc = [c; mean(lbp_acc)]
 end
 
@@ -60,7 +67,7 @@ end
 function features = sparse_encode(train_list, test_list, dict_size)
     features = struct('train', zeros(dict_size, length(train_list)), ...
                       'test', zeros(dict_size, length(test_list)));
-    param = struct('K', dict_size, 'lambda', 1, 'iter', 100);
+    param = struct('K', dict_size, 'lambda', 1, 'iter', 1000);
 
     train_vocabs = double([train_list.d]);
     test_vocabs = double([test_list.d]);
@@ -79,3 +86,20 @@ function features = sparse_encode(train_list, test_list, dict_size)
     end
 end
 
+function descriptor = extract_color_hist(image)
+    num_block = [4 4];
+    num_bin = 32;
+    descriptor = zeros(num_bin, prod(num_block), size(image, 3));
+
+    edges = linspace(0, 255, num_bin+1);
+    for color = 1:size(image, 3)
+        channel_image = image(:, :, color);
+        block_size = size(channel_image)./num_block;
+        block = im2col(channel_image, block_size, 'distinct');
+        for idx = 1:size(block, 2)
+            hist = histcounts(block(:, idx), edges);
+            descriptor(:, idx, color) = hist/norm(hist);
+        end
+    end
+    descriptor = reshape(descriptor, [], 1);
+end
--- matlab/kmeans_encode.m
+++ matlab/kmeans_encode.m
@@ -22,4 +22,13 @@ function features = kmeans_encode(train_list, test_list, dict_size)
     features.test = test_encode;
 end
 
+function hists = kmeans_hists(assignment, num_descriptors, dict_size)
+    hists = zeros(dict_size, length(num_descriptors));
+    offset = cumsum(num_descriptors)-num_descriptors;
+    for idx = 1:length(num_descriptors)
+        v = assignment(offset(idx)+1:offset(idx)+num_descriptors(idx));
+        hists(:, idx) = vl_ikmeanshist(dict_size, v);
+    end
+end
+
 
