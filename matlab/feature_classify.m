e84daedab615472f3cfb454b84a168dd3fed6e6c
zxm20243 <zxm20243@gmail.com>
2015-04-22 06:18:04 +0800

    rewrite linear classify

--- matlab/feature_classify.m
+++ matlab/feature_classify.m
@@ -4,24 +4,26 @@ function feature_classify(image_list)
 
     % Extract descriptors
     dataset = extract_descriptors(dataset);
-    %save('baseline.mat', '-v7.3');
+    save('baseline.mat', '-v7.3');
 
     % For each fold, generate features by descriptors
     num_fold = 5;
     folds = cross_validation(dataset, num_fold);
-    for v = 1:num_fold/num_fold
-        train_set = dataset(folds(v).train);
-        test_set = dataset(folds(v).test);
+    for v = 1:num_fold
+        train_set = folds(v).train;
+        test_set = folds(v).test;
         sc = @(x, d, p) mean(mexLasso(double(x), d, p), 2);
 
         % Encode SIFT descriptor
-        param = struct('K', 1024/512, 'lambda', 1, 'lambda2', 0, 'iter', 1000/1000, 'numThreads', -1);
-        sift_dict = mexTrainDL_Memory(double([train_set.sift]), param);
+        param = struct('K', 1024, 'lambda', 1, 'lambda2', 0, ...
+                       'iter', 1000, 'verbose', false, 'numThreads', -1);
+        sift_dict = mexTrainDL_Memory(double([dataset(train_set).sift]), param);
         sift = arrayfun(@(x) {sc(x.sift, sift_dict, param)}, dataset');
 
         % Encode LBP descriptor
-        param = struct('K', 2048/1024, 'lambda', 1, 'lambda2', 0, 'iter', 1000/1000, 'numThreads', -1);
-        lbp_dict = mexTrainDL_Memory(double([train_set.lbp]), param);
+        param = struct('K', 2048, 'lambda', 1, 'lambda2', 0, ...
+                       'iter', 1000, 'verbose', false, 'numThreads', -1);
+        lbp_dict = mexTrainDL_Memory(double([dataset(train_set).lbp]), param);
         lbp = arrayfun(@(x) {sc(x.lbp, lbp_dict, param)}, dataset);
         
         % Encode color histogram
@@ -31,29 +33,14 @@ function feature_classify(image_list)
         gabor = [dataset.gabor];
 
         % Classify by linear SVM
-        c = 10.^[2:-1:-7];
-%{
-        model = train(labels.train, sparse(features.train), ...
-                      ['-c ', num2str(c(idx)), ' -q'], 'col');
-        [~, acc, ~] = predict(labels.test, ...
-                              sparse(features.test), model, '-q', 'col');
-        acc_list(idx) = acc(1);
-%}
- 
-
-%{
-        sift_acc(v, :) = linear_classify(sift, labels, c);
-        lbp_acc(v, :) = linear_classify(lbp, labels, c);
-        color_acc(v, :) = linear_classify(color, labels, c);
-        gabor_acc(v, :) = linear_classify(gabor, labels, c);
-%}
+        sift_acc(v, :) = linear_classify([dataset.label], [sift{:}], folds(v));
+        lbp_acc(v, :) = linear_classify([dataset.label], [lbp{:}], folds(v));
+        color_acc(v, :) = linear_classify([dataset.label], color, folds(v));
+        gabor_acc(v, :) = linear_classify([dataset.label], gabor, folds(v));
     end
-%{
-    %save('baseline.mat', '-v7.3');
-    sift_acc = [c; mean(sift_acc)]
-    lbp_acc = [c; mean(lbp_acc)]
-    color_acc = [c; mean(color_acc)]
-    gabor_acc = [c; mean(gabor_acc)]
-%}
+    save('baseline.mat', '-v7.3');
+    sift_acc = [mean(sift_acc)]
+    lbp_acc = [mean(lbp_acc)]
+    color_acc = [mean(color_acc)]
+    gabor_acc = [mean(gabor_acc)]
 end
-
--- matlab/linear_classify.m
+++ matlab/linear_classify.m
@@ -1,10 +1,15 @@
-function acc_list = linear_classify(features, labels, c)
+function acc_list = linear_classify(label, feature, fold)
+    c = 10.^[2:-1:-7];
     acc_list = zeros(1, length(c));
+
+    train_inst = sparse(feature(:, fold.train)');
+    test_inst = sparse(feature(:, fold.test)');
+    train_label = double(label(fold.train)');
+    test_label = double(label(fold.test)');
+
     for idx = 1:length(c)
-        model = train(labels.train, sparse(features.train), ...
-                      ['-c ', num2str(c(idx)), ' -q'], 'col');
-        [~, acc, ~] = predict(labels.test, ...
-                              sparse(features.test), model, '-q', 'col');
+        model = train(train_label, train_inst, ['-c ', num2str(c(idx)), ' -q']);
+        [guess, acc, prob] = predict(test_label, test_inst, model, '-q');
         acc_list(idx) = acc(1);
     end
 end
