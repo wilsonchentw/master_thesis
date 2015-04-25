efdc53aa886f2dcd11529185ff195543a9646bb2
zxm20243 <zxm20243@gmail.com>
2015-04-25 18:23:18 +0800

    modify typo

--- matlab/baseline.m
+++ matlab/baseline.m
@@ -47,6 +47,8 @@ function baseline(image_list)
         inst = {sift_encode, lbp_encode, color_encode, gabor_encode};
         [vote, base, top_acc] = samme(label, inst, f);
 
+        vote
+        top_acc
         break
     end
 end
@@ -59,7 +61,7 @@ function [vote, base, top_acc] = samme(label, inst, fold)
     % Write subproblem for grid.py to search best parameter
     for idx = 1:length(inst)
         filename = ['feature_', num2str(idx), '.train'];
-        train_inst = sparse(inst{idx}(:, train_list)')
+        train_inst = sparse(inst{idx}(:, train_list)');
         libsvmwrite(filename, label(train_list), train_inst);
     end
     train_option = {'-c 1 -g 0.0010 -b 1 -q', '-c 1 -g 0.0005 -b 1 -q', ...
