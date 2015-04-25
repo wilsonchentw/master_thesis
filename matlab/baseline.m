35071bd686aa2773f310718820d7bb23927ff615
zxm20243 <zxm20243@gmail.com>
2015-04-25 18:18:52 +0800

    modify parameter

--- matlab/baseline.m
+++ matlab/baseline.m
@@ -20,9 +20,9 @@ function baseline(image_list)
         label = [dataset.label];
 
         % SIFT descriptors with sparse coding
-        sift = struct('dim', 1024/512, 'p', [], 'dict', [], 'alpha', [], 'n', []);
+        sift = struct('dim', 1024, 'p', [], 'dict', [], 'alpha', [], 'n', []);
         sift.p = struct('K', sift.dim, 'lambda', 1, 'lambda2', 0, ...
-                        'iter', 1000/1000, 'mode', 2, 'modeD', 0, ...
+                        'iter', 1000, 'mode', 2, 'modeD', 0, ...
                         'modeParam', 0, 'clean', true, 'numThreads', 4);
         sift.dict = mexTrainDL_Memory([dataset(f.train).sift], sift.p);
         sift.alpha = mexLasso([dataset.sift], sift.dict, sift.p);
@@ -30,9 +30,9 @@ function baseline(image_list)
         sift_encode = pooling(sift.alpha, sift.n);
 
         % LBP descriptors with sparse coding
-        lbp = struct('dim', 2048/512, 'p', [], 'dict', [], 'alpha', [], 'n', []);
+        lbp = struct('dim', 2048, 'p', [], 'dict', [], 'alpha', [], 'n', []);
         lbp.p = struct('K', lbp.dim, 'lambda', 1, 'lambda2', 0, ...
-                       'iter', 1000/1000, 'mode', 2, 'modeD', 0, ...
+                       'iter', 1000, 'mode', 2, 'modeD', 0, ...
                        'modeParam', 0, 'clean', true, 'numThreads', 4);
         lbp.dict = mexTrainDL_Memory([dataset(f.train).lbp], lbp.p);
         lbp.alpha = mexLasso([dataset.lbp], lbp.dict, lbp.p);
@@ -47,8 +47,6 @@ function baseline(image_list)
         inst = {sift_encode, lbp_encode, color_encode, gabor_encode};
         [vote, base, top_acc] = samme(label, inst, f);
 
-        vote
-        top_acc
         break
     end
 end
@@ -59,11 +57,11 @@ function [vote, base, top_acc] = samme(label, inst, fold)
     test_list = fold.test;
 
     % Write subproblem for grid.py to search best parameter
-    %for idx = 1:length(inst)
-    %    filename = ['feature_', num2str(idx), '.train'];
-    %    train_inst = sparse(inst{idx}(:, train_list)')
-    %    libsvmwrite(filename, label(train_list), train_inst);
-    %end
+    for idx = 1:length(inst)
+        filename = ['feature_', num2str(idx), '.train'];
+        train_inst = sparse(inst{idx}(:, train_list)')
+        libsvmwrite(filename, label(train_list), train_inst);
+    end
     train_option = {'-c 1 -g 0.0010 -b 1 -q', '-c 1 -g 0.0005 -b 1 -q', ...
                     '-c 1 -g 0.0007 -b 1 -q', '-c 1 -g 0.0010 -b 1 -q', };
     val_option = '-b 1 -q';
