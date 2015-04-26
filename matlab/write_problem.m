ca15be0ee9f4d4acaf7dc038cf5a62faff28d534
zxm20243 <zxm20243@gmail.com>
2015-04-27 07:37:39 +0800

    remove unnecessary file

--- matlab/write_problem.m
+++
@@ -1,9 +0,0 @@
-function write_problem(filename, label, feature, fold)
-    train_inst =  sparse(feature(:, fold.train)');
-    train_label = double(label(fold.train)');
-    test_inst = sparse(feature(:, fold.test)');
-    test_label = double(label(fold.test)');
-
-    libsvmwrite([filename, '_train.dat'], train_label, train_inst);
-    libsvmwrite([filename, '_test.dat'], test_label, test_inst);
-end
