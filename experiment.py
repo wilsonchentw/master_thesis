4da7a03e75a52cde7480f88b1382d15dfe77cb77
zxm20243 <zxm20243@gmail.com>
2015-05-18 23:06:43 +0800

    add MATLABPATH

--- experiment.py
+++ experiment.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from os import environ, pathsep
 from os import getenv, listdir, getcwd
-from os.path import realpath, isfile, dirname
+from os.path import normpath, realpath, basename, dirname, isfile, isdir
 import subprocess
 import sys
 
@@ -16,7 +16,10 @@ lib = {
     'vlfeat': "../vlfeat", 
     'libsvm': "../libsvm", 
     'liblinear': "../liblinear", 
-    'spams_py': "../spams/spams-python/install/lib/python2.7/site-packages", 
+    'spams-python': (
+        "../spams/spams-python/install/lib/python2.7/site-packages"
+    ), 
+    'spams-matlab': "../spams/spams-matlab"
 }
 
 
@@ -37,8 +40,8 @@ def setup_environment(lib):
             getenv('LD_PRELOAD'), 
             os.path.join(lib['gcc'], "libgfortran.so"), 
             os.path.join(lib['gcc'], "libgcc_s.so"), 
-            os.path.join(lib['gcc'], 'libstdc++.so'), 
-            os.path.join(lib['gcc'], 'libgomp.so'), 
+            os.path.join(lib['gcc'], "libstdc++.so"), 
+            os.path.join(lib['gcc'], "libgomp.so"), 
         ])
     )
     environ['PYTHONPATH'] = pathsep.join(
@@ -46,13 +49,22 @@ def setup_environment(lib):
             getenv('PYTHON'), 
             os.path.join(lib['libsvm'], "python"), 
             os.path.join(lib['liblinear'], "python"), 
-            os.path.join(lib['spams_py']), 
+            os.path.join(lib['spams-python']), 
+        ])
+    )
+    environ['MATLABPATH'] = pathsep.join(
+        filter(None, [
+            getenv('MATLABPATH'), 
+            os.path.join(lib['liblinear'], "matlab"), 
+            os.path.join(lib['libsvm'], "matlab"), 
+            os.path.join(lib['spams-matlab'], "test_release"), 
+            os.path.join(lib['spams-matlab'], "src_release"), 
+            os.path.join(lib['spams-matlab'], "build"), 
         ])
     )
 
 
 def generate_list(path, listname, percent):
-    prefix = path.rpartition(os.path.sep)[2]
     listname = ["{0}_{1}.list".format(prefix, name) for name in listname]
     if sum(percent) < 100:
         listname.append(os.devnull)
@@ -64,7 +76,6 @@ def generate_list(path, listname, percent):
 
 
 if __name__ == "__main__":
-
     # Setup argument parser
     parser = argparse.ArgumentParser()
     dataset = parser.add_mutually_exclusive_group(required=True)
@@ -73,18 +84,39 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if args.din is not None:
-        dataset = args.din.rpartition(os.path.sep)[2]
-        # Check if there is possible duplicate file by prefix of filename.
-        if any([isfile(f) and f.startswith(dataset) for f in listdir(".")]):
-            print "Find possible duplicate file, check your working directory"
+        cwd = getcwd()
+        din = normpath(args.din)
+        prefix = basename(din)
+
+        if not isdir(args.din):
+            print "Input is not directory"
+            exit(-1)
+        elif any([isfile(f) and f.startswith(prefix) for f in listdir(cwd)]):
+            print "Found possible duplicate file in `{0}`:".format(cwd)
+            for f in listdir(cwd):
+                if isfile(f) and f.startswith(prefix):
+                    print os.path.join(cwd, f)
             exit(-1)
         else:
-            print "Generate image list ..."
-            generate_list(args.din, ["small", "medium", "large"], [5, 20, 50])
-            generate_list(args.din, ["full"], [100])
+            print "Generate image list ... "
+            generate_list(din, ["small", "medium", "large"], [5, 20, 50])
+            generate_list(din, ["full"], [100])
             exit(0)
+    elif args.fin is not None:
+        if not isfile(args.fin):
+            print "Input is not file"
+            exit(-1)
+        else:
+            # If input is image list, setup environment variable
+            root = dirname(realpath(sys.argv[0]))
+            fin = realpath(args.fin)
+            setup_environment(lib)
 
+            vl_setup = os.path.join(lib['vlfeat'], "toolbox", "vl_setup.m")
+            baseline = os.path.join(root, 'baseline')
 
-    # If input is image list, setup environment variable
-    setup_environment(lib)
-    root = dirname(realpath(sys.argv[0]))
+            #run(fullfile(root_dir, '../vlfeat/toolbox/vl_setup'));
+            cmd = "addpath('{0}'); baseline {1}; quit".format(baseline, fin)
+            start_args = ["-nodesktop -nosplash -singleCompThread -r"]
+            #subprocess.call(cmd, stdout=sys.stdout, stderr=sys.stderr)
+
