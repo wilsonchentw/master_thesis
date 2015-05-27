25247366ac00703457cde7d7bffdcd73a1eb4424
zxm20243 <zxm20243@gmail.com>
2015-05-28 04:08:31 +0800

    Poor LBP performance

--- thesis/descriptor.py
+++ thesis/descriptor.py
@@ -40,37 +40,3 @@ def extract_phog(pathlist):
 def extract_color(pathlist):
     color = extract_descriptor(pathlist, get_color)
     return color.reshape(color.shape[0], -1)
-
-
-def extract_lbp(pathlist):
-    lbp = extract_descriptor(pathlist, get_lbp)
-    return lbp
-
-
-def get_lbp(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    lbps = pyramid_lbp(gray_image, 1, 8, 1)
-    return np.concatenate([lbp.reshape(-1) for lbp in lbps])
-
-
-def pyramid_lbp(image, level, point, radius):
-    bins, step = 59, np.array((radius, radius))
-
-    lbp_image = local_binary_pattern(image, point, radius, 'nri_uniform')
-    lbps = [lbp_hist(lbp_image, bins, step * 2, step)]
-    for lv in range(1, level):
-        image = cv2.pyrDown(image)
-        lbp_image = local_binary_pattern(image, point, radius, 'nri_uniform')
-        lbps.append(lbp_hist(lbp_image, bins, step * 2, step))
-
-    return lbps
-
-
-def lbp_hist(image, bins, block, step):
-    blocks = SlidingWindow(image.shape, block, step)
-    hist = np.zeros(np.append(blocks.dst_shape, bins))
-    for block in blocks:
-        patch = image[block].astype(int).reshape(-1)
-        hist[block.dst] = np.bincount(patch, minlength=bins)
-        hist[block.dst] = np.sqrt(hist[block.dst] / (np.sum(block.dst) + eps))
-    return hist
