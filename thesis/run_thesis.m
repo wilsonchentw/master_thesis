49000baef6e09436801ac282d599c28c30d6ed82
zxm20243 <zxm20243@gmail.com>
2015-06-04 20:29:23 +0800

    add kernel map

--- thesis/run_thesis.m
+++ thesis/run_thesis.m
@@ -1,16 +1,30 @@
 function run_thesis(image_list)
+
+    % -------------------------------------------------------------------------
+    % Parse image file list
+    % -------------------------------------------------------------------------
+
     setup_3rdparty(fullfile('~/Software'))
     [prefix, label, path] = parse_list(image_list);
+    folds = cross_validation(label, 5);
 
-    sift = extract_descriptor(path, 'sift');
+    % -------------------------------------------------------------------------
+    % Feature Extraction
+    % -------------------------------------------------------------------------
+
+    %sift = extract_descriptor(path, 'sift');
     %lbp = extract_descriptor(path, 'lbp');
     %hog = extract_descriptor(path, 'hog');
     %phow = extract_descriptor(path, 'phow');
 
-    lbp_feature = [lbp{:, :}];
-    lbp_feature = cell2mat(reshape(lbp_feature, 1, []));
-    lbp_feature = reshape(lbp_feature, [], size(lbp, 2));
-    train(double(label), sparse(double(lbp_feature)), '-v 5 -q', 'col');
+    % -------------------------------------------------------------------------
+    % Directly Training
+    % -------------------------------------------------------------------------
+
+    %lbp_feature = [lbp{:, :}];
+    %lbp_feature = cell2mat(reshape(lbp_feature, 1, []));
+    %lbp_feature = reshape(lbp_feature, [], size(lbp, 2));
+    %train(double(label), sparse(double(lbp_feature)), '-v 5 -q', 'col');
 
     %hog_feature = reshape(hog, 1, 1, []);
     %hog_feature = reshape(cell2mat(hog_feature), [], size(hog, 2));
@@ -23,8 +37,34 @@ function run_thesis(image_list)
 
     %save([prefix, '.mat'], 'sift', 'lbp')
 
+    % -------------------------------------------------------------------------
+    % Encoding
+    % -------------------------------------------------------------------------
 
+    train_idx = folds(1).train;
+    test_idx = folds(1).test;
 
+    % Generate K-means codebook
+    dict_size = 2048;
+    train_sift = cell2mat(sift(train_idx));
+    sift_dict = vl_ikmeans(train_sift, dict_size, 'method', 'elkan');
+
+    % Conduct bag-of-word
+    sift_bow = zeros(dict_size, length(sift));
+    for idx = 1:length(sift)
+        asgn = vl_ikmeanspush(sift{idx}, sift_dict);
+        hist = vl_ikmeanshist(dict_size, asgn);
+        sift_bow(:, idx) = double(hist) / sum(hist);
+    end
+
+    % Approximated chi-square kernel mapping
+    sift_bow = vl_homkermap(sift_bow, 2, 'kernel', 'kchi2');
+
+    % Evaluate with linear svm
+    train_sift = sparse(sift_bow(:, train_idx));
+    test_sift = sparse(sift_bow(:, test_idx));
+    model = train(double(label(train_idx)), train_sift, '-q', 'col');
+    predict(double(label(test_idx)), test_sift, model, '', 'col');
 end
 
 function setup_3rdparty(root_dir)
@@ -72,22 +112,51 @@ function image = read_image(path)
     image = normalize_image(raw_image, norm_size);
 end
 
+function folds = cross_validation(label, num_fold)
+    folds(1:num_fold) = struct('train', [], 'test', []);
+    categories = unique(label);
+    for c = 1:length(categories)
+        % Select particular category
+        list = find(label == categories(c));
+        len = length(list);
+
+        % Calculate #test_case on each fold, exactly cover all instances
+        sample_fold = randsample(num_fold, mod(len, num_fold));
+        test_nums(1:num_fold) = floor(len/num_fold);
+        test_nums(sample_fold) = floor(len/num_fold)+1;
+        test_nums = test_nums - (test_nums==len);  % Ensure #train_instance > 0
+
+        % Distribute all instances to training set and testing set
+        list = list(randperm(len));
+        for v = 1:num_fold
+            test_list = list(1:test_nums(v));
+            train_list = list(test_nums(v)+1:end);
+
+            folds(v).train = [folds(v).train; train_list];
+            folds(v).test = [folds(v).test; test_list];
+            list = [train_list; test_list];
+        end
+    end
+end
+
 function descriptor = extract_descriptor(path, ds_type)
     descriptors = cell(1, length(path));
     for idx = 1:length(path)
         image = read_image(path{idx});
         gray_image = rgb2gray(image);
 
-        if strcmp(ds_type, 'sift')
-            descriptor{idx} = get_sift(single(gray_image));
-        elseif strcmp(ds_type, 'lbp')
-            descriptor{idx} = get_pyramid_lbp(single(gray_image));
-        elseif strcmp(ds_type, 'hog')
-            descriptor{idx} = get_hog(single(image));
-        elseif strcmp(ds_type, 'phow')
-            descriptor{idx} = get_phow(im2single(image));
-        else
-            fprintf(1, 'Wrong descriptor name.');
+        switch ds_type
+            case 'sift'
+                descriptor{idx} = get_sift(single(gray_image));
+            case 'lbp'
+                descriptor{idx} = get_pyramid_lbp(single(gray_image));
+            case 'hog'
+                descriptor{idx} = get_hog(single(image));
+            case 'phow'
+                descriptor{idx} = get_phow(im2single(image));
+            otherwise
+                fprintf(1, 'Wrong descriptor name.');
+                return;
         end
     end
 end
